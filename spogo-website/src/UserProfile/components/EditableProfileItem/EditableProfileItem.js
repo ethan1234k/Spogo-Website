d1ee6c7fe6f8e0ff8c3fc623b5888802deb44060
ethan1234k <41340975+ethan1234k@users.noreply.github.com>
2022-03-11 10:21:32 -0800

    Revert "update year info for 2022"

--- spogo-website/src/UserProfile/components/EditableProfileItem/EditableProfileItem.js
+++ spogo-website/src/UserProfile/components/EditableProfileItem/EditableProfileItem.js
@@ -475,7 +475,6 @@ const EditableProfileItem = (props) => {
 
   const getYearIndex = (year) => {
     let yearArray = [
-      "2022",
       "2021",
       "2020",
       "2019",
@@ -782,29 +781,28 @@ const EditableProfileItem = (props) => {
                           <option selected hidden value="0">
                             Year
                           </option>
-                          <option value="1">2022</option>
-                          <option value="2">2021</option>
-                          <option value="3">2020</option>
-                          <option value="4">2019</option>
-                          <option value="5">2018</option>
-                          <option value="6">2017</option>
-                          <option value="7">2016</option>
-                          <option value="8">2015</option>
-                          <option value="9">2014</option>
-                          <option value="10">2013</option>
-                          <option value="11">2012</option>
-                          <option value="12">2011</option>
-                          <option value="13">2010</option>
-                          <option value="14">2009</option>
-                          <option value="15">2008</option>
-                          <option value="16">2007</option>
-                          <option value="17">2006</option>
-                          <option value="18">2005</option>
-                          <option value="19">2004</option>
-                          <option value="20">2003</option>
-                          <option value="21">2002</option>
-                          <option value="22">2001</option>
-                          <option value="23">2000</option>
+                          <option value="1">2021</option>
+                          <option value="2">2020</option>
+                          <option value="3">2019</option>
+                          <option value="4">2018</option>
+                          <option value="5">2017</option>
+                          <option value="6">2016</option>
+                          <option value="7">2015</option>
+                          <option value="8">2014</option>
+                          <option value="9">2013</option>
+                          <option value="10">2012</option>
+                          <option value="11">2011</option>
+                          <option value="12">2010</option>
+                          <option value="13">2009</option>
+                          <option value="14">2008</option>
+                          <option value="15">2007</option>
+                          <option value="16">2006</option>
+                          <option value="17">2005</option>
+                          <option value="18">2004</option>
+                          <option value="19">2003</option>
+                          <option value="20">2002</option>
+                          <option value="21">2001</option>
+                          <option value="22">2000</option>
                         </select>
                       </div>
                       {invalidExperienceStartDate && (
@@ -869,29 +867,28 @@ const EditableProfileItem = (props) => {
                               <option selected hidden value="0">
                                 Year
                               </option>
-                              <option value="1">2022</option>
-                              <option value="2">2021</option>
-                              <option value="3">2020</option>
-                              <option value="4">2019</option>
-                              <option value="5">2018</option>
-                              <option value="6">2017</option>
-                              <option value="7">2016</option>
-                              <option value="8">2015</option>
-                              <option value="9">2014</option>
-                              <option value="10">2013</option>
-                              <option value="11">2012</option>
-                              <option value="12">2011</option>
-                              <option value="13">2010</option>
-                              <option value="14">2009</option>
-                              <option value="15">2008</option>
-                              <option value="16">2007</option>
-                              <option value="17">2006</option>
-                              <option value="18">2005</option>
-                              <option value="19">2004</option>
-                              <option value="20">2003</option>
-                              <option value="21">2002</option>
-                              <option value="22">2001</option>
-                              <option value="23">2000</option>
+                              <option value="1">2021</option>
+                              <option value="2">2020</option>
+                              <option value="3">2019</option>
+                              <option value="4">2018</option>
+                              <option value="5">2017</option>
+                              <option value="6">2016</option>
+                              <option value="7">2015</option>
+                              <option value="8">2014</option>
+                              <option value="9">2013</option>
+                              <option value="10">2012</option>
+                              <option value="11">2011</option>
+                              <option value="12">2010</option>
+                              <option value="13">2009</option>
+                              <option value="14">2008</option>
+                              <option value="15">2007</option>
+                              <option value="16">2006</option>
+                              <option value="17">2005</option>
+                              <option value="18">2004</option>
+                              <option value="19">2003</option>
+                              <option value="20">2002</option>
+                              <option value="21">2001</option>
+                              <option value="22">2000</option>
                             </select>
                           </>
                         ) : (
@@ -1158,29 +1155,28 @@ const EditableProfileItem = (props) => {
                           <option selected hidden value="0">
                             Year
                           </option>
-                          <option value="1">2022</option>
-                          <option value="2">2021</option>
-                          <option value="3">2020</option>
-                          <option value="4">2019</option>
-                          <option value="5">2018</option>
-                          <option value="6">2017</option>
-                          <option value="7">2016</option>
-                          <option value="8">2015</option>
-                          <option value="9">2014</option>
-                          <option value="10">2013</option>
-                          <option value="11">2012</option>
-                          <option value="12">2011</option>
-                          <option value="13">2010</option>
-                          <option value="14">2009</option>
-                          <option value="15">2008</option>
-                          <option value="16">2007</option>
-                          <option value="17">2006</option>
-                          <option value="18">2005</option>
-                          <option value="19">2004</option>
-                          <option value="20">2003</option>
-                          <option value="21">2002</option>
-                          <option value="22">2001</option>
-                          <option value="23">2000</option>
+                          <option value="1">2021</option>
+                          <option value="2">2020</option>
+                          <option value="3">2019</option>
+                          <option value="4">2018</option>
+                          <option value="5">2017</option>
+                          <option value="6">2016</option>
+                          <option value="7">2015</option>
+                          <option value="8">2014</option>
+                          <option value="9">2013</option>
+                          <option value="10">2012</option>
+                          <option value="11">2011</option>
+                          <option value="12">2010</option>
+                          <option value="13">2009</option>
+                          <option value="14">2008</option>
+                          <option value="15">2007</option>
+                          <option value="16">2006</option>
+                          <option value="17">2005</option>
+                          <option value="18">2004</option>
+                          <option value="19">2003</option>
+                          <option value="20">2002</option>
+                          <option value="21">2001</option>
+                          <option value="22">2000</option>
                         </select>
                       </div>
                       {invalidAccomplishmentDateReceived && (
